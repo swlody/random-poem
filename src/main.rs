3b86448f0460c8e5db85bf3769104a3246c5bd2c
Sam Wlody <swlody@gmail.com>
2024-07-31 19:53:26 -0400

    Convert to shuttle

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,14 @@ mod query;
 
 use sqlx::SqlitePool;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .init();
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let db = SqlitePool::connect("sqlite://poems.db")
+        .await
+        .map_err(|e| shuttle_runtime::Error::Database(e.to_string()))?;
+    let app = query::routes().with_state(db);
 
-    let db = SqlitePool::connect("sqlite://poems.db").await?;
-    let app = query::routes();
-    let app = app.with_state(db.clone());
+    Ok(app.into())
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:5050").await?;
-    axum::serve(listener, app).await?;
-
-    db.close().await;
-    Ok(())
+    // db.close().await;
 }
