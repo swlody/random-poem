30c9310da87093ee9371b5d10e2d3cb27375bc78
Sam Wlody <swlody@gmail.com>
2024-08-07 01:56:08 -0400

    Fix request_id setting

--- src/layers.rs
+++ src/layers.rs
@@ -1,11 +1,10 @@
-use axum::{
-    http::{HeaderName, Request},
-    Router,
-};
+use axum::{http::Request, Router};
 use sentry::integrations::tower::{NewSentryLayer, SentryHttpLayer};
+use tower::ServiceBuilder;
 use tower_http::{
-    request_id::{MakeRequestId, PropagateRequestIdLayer, RequestId, SetRequestIdLayer},
+    request_id::{MakeRequestId, RequestId},
     trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer},
+    ServiceBuilderExt as _,
 };
 use uuid::Uuid;
 
@@ -21,8 +20,8 @@ impl MakeRequestId for MakeRequestUuidV7 {
     fn make_request_id<B>(&mut self, _request: &Request<B>) -> Option<RequestId> {
         // Use UUIDv7 so that request ID can be sorted by time
         let request_id = Uuid::now_v7();
-        // TODO It's more appropriate for this to happen in with_sentry_layer()
-        // set it using the x-request-id header?
+        // TODO This is more appropriately addded along with other sentry layers.
+        // Create a new middleware to read x-request-id and add it to sentry scope.
         sentry::configure_scope(|scope| {
             scope.set_tag("request_id", request_id);
         });
@@ -33,20 +32,21 @@ impl MakeRequestId for MakeRequestUuidV7 {
 impl AddLayers for Router {
     fn with_tracing_layer(self) -> Self {
         // Enables tracing for each request and adds a request ID header to resposne
-        self.layer(
-            TraceLayer::new_for_http()
-                .make_span_with(DefaultMakeSpan::new().include_headers(true))
-                .on_response(DefaultOnResponse::new().include_headers(true)),
-        )
-        .layer(SetRequestIdLayer::new(
-            HeaderName::from_static("x-request-id"),
-            MakeRequestUuidV7,
-        ))
-        .layer(PropagateRequestIdLayer::x_request_id())
+        let tracing_service = ServiceBuilder::new()
+            .layer(
+                TraceLayer::new_for_http()
+                    .make_span_with(DefaultMakeSpan::new().include_headers(true))
+                    .on_response(DefaultOnResponse::new().include_headers(true)),
+            )
+            .set_x_request_id(MakeRequestUuidV7)
+            .propagate_x_request_id();
+        self.layer(tracing_service)
     }
 
     fn with_sentry_layer(self) -> Self {
-        self.layer(NewSentryLayer::new_from_top())
-            .layer(SentryHttpLayer::with_transaction())
+        let sentry_service = ServiceBuilder::new()
+            .layer(NewSentryLayer::new_from_top())
+            .layer(SentryHttpLayer::with_transaction());
+        self.layer(sentry_service)
     }
 }
