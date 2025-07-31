3c5e04c26edc31dd5990074497c36ca1b55c7156
Sam Wlody <swlody@gmail.com>
2025-07-31 19:19:47 -0400

    remove sentry integration

--- Dockerfile
+++ Dockerfile
@@ -11,26 +11,14 @@ COPY --from=planner /app/recipe.json recipe.json
 RUN cargo chef cook --release --recipe-path recipe.json
 # Build application
 COPY . .
-RUN cargo build --release --bin dial-a-poem
-
-# Create debug info
-RUN objcopy --only-keep-debug --compress-debug-sections=zlib /app/target/release/dial-a-poem /app/target/release/dial-a-poem.debug
-RUN objcopy --strip-debug --strip-unneeded /app/target/release/dial-a-poem
-RUN objcopy --add-gnu-debuglink=/app/target/release/dial-a-poem.debug /app/target/release/dial-a-poem
-
-RUN curl -sL https://sentry.io/get-cli | bash
-
-RUN mv /app/target/release/dial-a-poem.debug /app
-RUN --mount=type=secret,id=sentry_auth_token \
-    sentry-cli debug-files upload --include-sources --org sam-wlody --project dial-a-poem --auth-token $(cat /run/secrets/sentry_auth_token) /app/dial-a-poem.debug
-RUN rm /app/dial-a-poem.debug
+RUN cargo build --release --bin random-poem
 
 # We do not need the Rust toolchain to run the binary!
 FROM debian:bookworm-slim AS runtime
 WORKDIR /app
-COPY --from=builder /app/target/release/dial-a-poem /usr/local/bin
+COPY --from=builder /app/target/release/random-poem /usr/local/bin
 
 COPY poems.sqlite3 /app/poems.sqlite3
 COPY assets/static/ /app/assets/static
 
-ENTRYPOINT ["/usr/local/bin/dial-a-poem"]
+ENTRYPOINT ["/usr/local/bin/random-poem"]
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,7 @@ use std::str::FromStr as _;
 
 use anyhow::Result;
 use axum::{serve, Router};
-use middleware::{MakeRequestUuidV7, SentryReportRequestInfoLayer};
-use secrecy::ExposeSecret;
-use sentry::integrations::tower::{NewSentryLayer, SentryHttpLayer};
+use middleware::MakeRequestUuidV7;
 use sqlx::SqlitePool;
 use tokio::net::TcpListener;
 use tower::ServiceBuilder;
@@ -20,76 +18,27 @@ use tower_http::{
     ServiceBuilderExt as _,
 };
 use tracing::Level;
-use tracing_subscriber::{layer::SubscriberExt as _, util::SubscriberInitExt as _};
+use tracing_subscriber::util::SubscriberInitExt as _;
 
 use crate::errors::serve_404;
-
-struct Config {
-    log_level: Option<tracing::Level>,
-    trace_sample_rate: Option<f32>,
-    error_sample_rate: Option<f32>,
-    sentry_dsn: secrecy::SecretString,
-}
-
-impl Config {
-    fn from_env() -> Self {
-        Self {
-            log_level: std::env::var("DAP_LOG_LEVEL").ok().map(|level| {
-                Level::from_str(level.as_str())
-                    .unwrap_or_else(|_| panic!("Invalid value for DAP_LOG_LEVEL: {level}"))
-            }),
-
-            trace_sample_rate: std::env::var("DAP_SENTRY_TRACING_SAMPLE_RATE")
-                .ok()
-                .map(|rate| {
-                    let rate = rate.parse().unwrap_or_else(|_| {
-                        panic!("Invalid value for DAP_SENTRY_TRACING_SAMPLE_RATE: {rate}")
-                    });
-                    assert!((0.0..=1.0).contains(&rate));
-                    rate
-                }),
-
-            error_sample_rate: std::env::var("DAP_SENTRY_ERROR_SAMPLE_RATE")
-                .ok()
-                .map(|rate| {
-                    let rate = rate.parse().unwrap_or_else(|_| {
-                        panic!("Invalid value for DAP_SENTRY_ERROR_SAMPLE_RATE: {rate}")
-                    });
-                    assert!((0.0..=1.0).contains(&rate));
-                    rate
-                }),
-
-            sentry_dsn: secrecy::SecretString::from(
-                std::env::var("SENTRY_DSN").expect("Missing SENTRY_DSN"),
-            ),
-        }
-    }
-}
 
 fn main() -> Result<()> {
     rubenvy::rubenvy_auto()?;
 
-    let config = Config::from_env();
+    let log_level = std::env::var("RP_LOG_LEVEL")
+        .ok()
+        .map_or(Level::DEBUG, |level| {
+            Level::from_str(level.as_str())
+                .unwrap_or_else(|_| panic!("Invalid value for RP_LOG_LEVEL: {level}"))
+        });
 
     // Initialize tracing subscribe
     tracing_subscriber::fmt()
         .with_target(true)
-        .with_max_level(config.log_level.unwrap_or(Level::DEBUG))
+        .with_max_level(log_level)
         .finish()
-        .with(sentry::integrations::tracing::layer())
         .try_init()
         .expect("Unable to initialize tracing_subscriber");
-
-    let _guard = sentry::init((
-        config.sentry_dsn.expose_secret(),
-        sentry::ClientOptions {
-            release: sentry::release_name!(),
-            traces_sample_rate: config.trace_sample_rate.unwrap_or(0.1),
-            sample_rate: config.error_sample_rate.unwrap_or(1.0),
-            attach_stacktrace: true,
-            ..Default::default()
-        },
-    ));
 
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
@@ -113,9 +62,6 @@ async fn run() -> Result<()> {
         .layer(
             ServiceBuilder::new()
                 .set_x_request_id(MakeRequestUuidV7)
-                .layer(NewSentryLayer::new_from_top())
-                .layer(SentryHttpLayer::with_transaction())
-                .layer(SentryReportRequestInfoLayer)
                 .layer(
                     TraceLayer::new_for_http()
                         .make_span_with(DefaultMakeSpan::new().include_headers(true))
